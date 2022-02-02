2da7667a0c0da4d814190ced6f4b8faade666b7f
YakinHabib <yakinhabibharahap@gmail.com>
2022-02-02 16:30:41 +0700

    Mengubah isi ketiga file

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-var tombolMenu = document.getElementsByClassName('tombol-menu')[0];
-var menu = document.getElementsByClassName('menu')[0];
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
 
-tombolMenu.onclick = function() {
-    menu.classList.toggle('active');
-}
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
 
-menu.onclick = function() {
-    menu.classList.toggle('active');
-}
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
